abca0fd6130d8c3dc732d3dc7af01495bc335700
zalwicker2 <zalwicker@gmail.com>
2022-12-16 17:39:35 -0600

    Updated stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectSpotlightComponent } from './home/project-spotlight/project-spotlight.component';
+import { CtaButtonsComponent } from './home/cta-buttons/cta-buttons.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    ProjectSpotlightComponent,
+    CtaButtonsComponent,
+    SocialLinksComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +6,5 @@ import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  faGithub = faGithub;
-  faInstagram = faInstagram;
-  faLinkedIn = faLinkedin
+  
 }
